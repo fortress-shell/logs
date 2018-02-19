b9840abe46f463fa4cc048ae0cce9ae7321cc945
Mikhail Faraponov <mikefaraponov@fortress-1.local>
2018-02-20 01:25:55 +0200

    Updated kafka options

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ const io = require('src/resources/io');
 const client = require('src/resources/kafka');
 const db = require('src/resources/db');
 const config = require('src/config');
-const options = config.get('kafka-options');
-const topics = [{
-    topic: options.get('KAFKA_TOPIC'),
-}];
+const {options, topics} = config.get('kafka');
 const consumer = new HighLevelConsumer(client, topics, options);
 const logs = new LogsController(io, db);
 
 consumer.on('message', logs.log.bind(logs));
-
 consumer.on('error', console.log);
 
 function onShutdown() {
