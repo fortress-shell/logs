6a615b1c1b9d7d6636aaa53db1c6d3d749557cf9
Mikhail Faraponov <mikhail.faraponov@sigma.software>
2018-02-20 11:57:52 +0200

    Updated

--- src/resources/db.js
+++ src/resources/db.js
@@ -3,6 +3,7 @@ const Sequalize = require('sequalize');
 const config = require('src/config');
 const DB_URL = config.get('DB_URL');
 
-module.exports = new Sequelize('mysql://localhost', {
+module.exports = new Sequelize(DB_URL, {
   dialect: 'mysql',
+  operatorsAliases: false,
 });
