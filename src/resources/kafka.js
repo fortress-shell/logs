ce58fc28e437ee7e14abce1fa7883f7d154012d8
Mikhail Faraponov <mikefaraponov@fortress-1.local>
2018-02-27 00:57:16 +0200

    Added id of kafka consumer

--- src/resources/kafka.js
+++ src/resources/kafka.js
@@ -1,10 +1,12 @@
 'use strict';
 const {ConsumerGroupStream} = require('kafka-node');
+const uuidv4 = require('uuid/v4');
 const config = require('src/config');
 const KAFKA_URL = config.get('KAFKA_URL');
 const {options, datacenters} = config.get('kafka');
 const consumerOptions = Object.assign(options, {
   kafkaHost: KAFKA_URL,
+  id: uuidv4()
 });
 
 function kafkaStream() {
